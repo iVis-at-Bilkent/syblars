4817ae344ca4cceeea148176a57eba5c0027bd4e
Hasan Balcı <balcihasan99@gmail.com>
2021-08-10 16:36:37 +0300

    Fix typos

--- public/js/main.menu.js
+++ public/js/main.menu.js
@@ -277,7 +277,7 @@ imageWidth.onkeydown = function(e) {
 let imageHeight = document.getElementById('imageHeight');
 imageHeight.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106)
-      || (e.keyCode > 47 && e.keyCode < 58
+      || (e.keyCode > 47 && e.keyCode < 58)
       || e.keyCode == 8)) {
         return false;
     }
